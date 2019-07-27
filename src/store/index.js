091da27a3bae058c5fc26dd82131b8edbfe319c3
antheas <antheas@users.noreply.github.com>
2019-07-27 21:43:45 +0300

    Setup basic store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// Redux persist
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Apply logger and thunk
+let middleware = { thunk };
+if (process.env.NODE_ENV !== "production") {
+  middleware = { ...middleware, logger };
+}
+
+const store = createStore(persistedReducer, applyMiddleware(...middleware));
+const persistor = persistStore(store);
 
 export default store;
+export { persistor };
