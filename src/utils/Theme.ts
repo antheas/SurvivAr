6fe21279478a348e521ff7b3c9b04d5e75c0a2d3
antheas <antheas@users.noreply.github.com>
2019-07-28 20:17:24 +0300

    Add theme structure with blue primary

---
+++ src/utils/Theme.ts
@@ -0,0 +1,94 @@
+import {
+  Dimensions,
+  Platform,
+  PixelRatio,
+  StyleSheet,
+  ColorPropType
+} from "react-native";
+
+// https://stackoverflow.com/questions/33628677/react-native-responsive-font-size
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+// based on iphone 5s's scale
+const scale = SCREEN_WIDTH / 320;
+
+export function normalize(size): number {
+  const newSize = size * scale;
+  if (Platform.OS === "ios") {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
+}
+
+export const colors = {
+  primary: "#0b3e8e",
+  primaryLight: "#001960",
+  primaryDark: "#4f68bf",
+
+  accent: "0b3e8e",
+  accentLight: "001960",
+  accentDark: "4f68bf",
+
+  textDark: "#131516",
+  text: "#373D3F",
+  textLight: "#707C80",
+  primaryText: "#ffffff",
+  accentText: "#ffffff",
+
+  white: "#ffffff"
+};
+
+export const text = {
+  size: StyleSheet.create({
+    mini: {
+      fontSize: normalize(12)
+    },
+    small: {
+      fontSize: normalize(15)
+    },
+    medium: {
+      fontSize: normalize(17)
+    },
+    large: {
+      fontSize: normalize(20)
+    },
+    xlarge: {
+      fontSize: normalize(24)
+    }
+  }),
+  color: StyleSheet.create({
+    primary: {
+      color: colors.primaryText
+    },
+    accent: {
+      color: colors.accentText
+    },
+    normal: {
+      color: colors.text
+    },
+    light: {
+      color: colors.textLight
+    },
+    dark: {
+      color: colors.textDark
+    }
+  })
+};
+
+export const component = {
+  container: StyleSheet.create({
+    background: {
+      flex: 1,
+      backgroundColor: colors.primaryDark
+    },
+    foreground: {
+      flex: 1,
+      backgroundColor: colors.white
+    },
+    accented: {
+      flex: 1,
+      backgroundColor: colors.accent
+    }
+  })
+};
