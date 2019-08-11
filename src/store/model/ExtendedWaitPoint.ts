7db303b4ffbad22f991b4bfeb8f56a15a14303ef
antheas <antheas@users.noreply.github.com>
2019-08-11 04:47:26 +0300

    Prevent showing invalid times

--- src/store/model/ExtendedWaitPoint.ts
+++ src/store/model/ExtendedWaitPoint.ts
@@ -9,6 +9,10 @@ export class ExtendedWaitPoint extends ExtendedPoint implements WaitPoint {
     super(p, distance);
     this.duration = p.duration;
     this.completedDuration = progress ? (progress.elapsedTime as number) : 0;
+
+    // Make sure we don't exceed duration
+    if (this.completedDuration > this.duration)
+      this.completedDuration = this.duration;
   }
 
   public get completed(): boolean {
