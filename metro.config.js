3ec692e13a28f83241ae28a82ade0a48c3a9f2b8
antheas <antheas@users.noreply.github.com>
2019-08-11 22:50:55 +0300

    Add proper blacklist to metro to fix crashes

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,26 @@
  *
  * @format
  */
+const blacklist = require("metro-config/src/defaults/blacklist");
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+        inlineRequires: false
+      }
+    })
   },
+  resolver: {
+    // Without this android builds crash packager
+    blacklistRE: blacklist([
+      // Ignore IntelliJ directories
+      /.*\.idea\/.*/,
+      // ignore git directories
+      /.*\.git\/.*/,
+      // Ignore android directories
+      /.*\/app\/build\/.*/
+    ])
+  }
 };
