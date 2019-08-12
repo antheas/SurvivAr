8552c3cfe266dec1a88e1307c9c36f2f4fdaf2ed
antheas <antheas@users.noreply.github.com>
2019-08-12 23:19:28 +0300

    Separate notification code

--- android/app/src/main/java/gr/tuc/explorar/location/BackgroundLocationService.java
+++ android/app/src/main/java/gr/tuc/explorar/location/BackgroundLocationService.java
@@ -1,6 +1,5 @@
 package gr.tuc.explorar.location;
 
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
@@ -8,16 +7,12 @@ import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
 
 import gr.tuc.explorar.MainActivity;
-import gr.tuc.explorar.R;
 
 public class BackgroundLocationService extends Service {
 
-  private static final int LOCATION_NOTIFICATION_ID = 345657543;
+  private LocationNotificationManager manager;
 
   @Override
   public void onCreate() {
@@ -26,31 +21,16 @@ public class BackgroundLocationService extends Service {
 
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
-    Intent notificationIntent = new Intent(this, MainActivity.class);
-    PendingIntent pendingIntent =
-            PendingIntent.getActivity(this, 0, notificationIntent, 0);
+    PendingIntent openAppIntent = PendingIntent.getActivity(
+            this,
+            0,
+            new Intent(this, MainActivity.class),
+            0);
 
-    String channelId = NotificationUtils.createNotificationChannel(this);
+    manager = new LocationNotificationManager(this, openAppIntent, null, null);
+    manager.startForeground(this);
 
-    NotificationCompat.Builder builder =
-            new NotificationCompat.Builder(this, channelId)
-                    .setContentTitle(getText(R.string.location_service_notification_title))
-                    .setContentText(getText(R.string.location_service_notification_content))
-                    .setSmallIcon(R.drawable.location_notification_icon)
-                    .setColor(ContextCompat.getColor(this, R.color.primary_color))
-                    .setContentIntent(pendingIntent);
-
-    Notification notification = builder.build();
-    startForeground(LOCATION_NOTIFICATION_ID, notification);
-
-    NotificationManagerCompat manager = NotificationManagerCompat.from(this);
-
-    new Handler().postDelayed(() -> {
-      Notification notification2 = builder.setContentText("update").build();
-      manager.notify(LOCATION_NOTIFICATION_ID, notification2);
-    }, 3000);
-
-    new Handler().postDelayed(this::stopSelf, 10000);
+    new Handler().postDelayed(this::stopSelf, 20000);
 
     return START_STICKY;
   }
